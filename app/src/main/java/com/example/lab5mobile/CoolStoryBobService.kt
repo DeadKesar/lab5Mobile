ea2bac7e281560f3c2e43fb4da45498918bb0dcb
DeadKesar <50805926+DeadKesar@users.noreply.github.com>
2025-03-26 20:14:14 +0300

    нипанятна

--- app/src/main/java/com/example/lab5mobile/CoolStoryBobService.kt
+++ app/src/main/java/com/example/lab5mobile/CoolStoryBobService.kt
@@ -1,10 +1,12 @@
 package com.example.lab5mobile
 
+import android.app.Notification
 import android.app.Service
 import android.content.Intent
 import android.os.Binder
 import android.os.IBinder
 import android.util.Log
+import androidx.core.app.NotificationCompat
 import kotlinx.coroutines.channels.BufferOverflow
 import kotlinx.coroutines.flow.MutableSharedFlow
 import kotlinx.coroutines.flow.SharedFlow
@@ -24,18 +26,21 @@ class CoolStoryBobService : Service(), CoolStoryBob {
     inner class LocalBinder : Binder() {
         fun getBob(): CoolStoryBob = this@CoolStoryBobService
     }
+
     private val binder = LocalBinder()
     private val _stories = MutableSharedFlow<String>(
         extraBufferCapacity = 1, onBufferOverflow = BufferOverflow.DROP_OLDEST
     )
     override val stories: SharedFlow<String> = _stories.asSharedFlow()
 
-    override fun onStartCommand(intent: Intent?, flags: Int, startId: Int): Int
-    {
+    override fun onStartCommand(intent: Intent?, flags: Int, startId: Int): Int {
+        // установка Foreground service
+        val notification = createNotification()
+        startForeground(1, notification)
+
         // имитация работы в потоке
         Thread {
-            repeat(10) {
-                data ->
+            repeat(10) { data ->
                 // каждую секунду отправляем историю
                 _stories.tryEmit("Story $data")
                 Thread.sleep(1000L)
@@ -45,6 +50,15 @@ class CoolStoryBobService : Service(), CoolStoryBob {
         }.start()
         return START_STICKY
     }
+
+    private fun createNotification(): Notification {
+        return NotificationCompat.Builder(this, CoolStoryBobNotification.CHANNEL_ID)
+            .setSmallIcon(R.drawable.baseline_cookie_24)
+            .setShowWhen(true) .setOngoing(true)
+            .setContentTitle("CoolStoryBobService started")
+            .setContentText("Story from bob") .build()
+    }
+
     override fun onBind(intent: Intent?): IBinder {
         return binder
     }
