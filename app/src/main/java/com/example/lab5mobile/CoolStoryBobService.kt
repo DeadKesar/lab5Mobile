53b5dd883d1f4c7aea886018a39c056ccb3dd2db
DeadKesar <50805926+DeadKesar@users.noreply.github.com>
2025-03-26 18:40:11 +0300

    2

--- app/src/main/java/com/example/lab5mobile/CoolStoryBobService.kt
+++ app/src/main/java/com/example/lab5mobile/CoolStoryBobService.kt
@@ -2,26 +2,53 @@ package com.example.lab5mobile
 
 import android.app.Service
 import android.content.Intent
+import android.os.Binder
 import android.os.IBinder
 import android.util.Log
+import kotlinx.coroutines.channels.BufferOverflow
+import kotlinx.coroutines.flow.MutableSharedFlow
+import kotlinx.coroutines.flow.SharedFlow
+import kotlinx.coroutines.flow.asSharedFlow
 
-class CoolStoryBobService : Service() {
+
+// Интерфейс, чтобы ограничить возможности из вне
+interface CoolStoryBob {
+    // будет поток историй
+    val stories: SharedFlow<String>
+}
+
+class CoolStoryBobService : Service(), CoolStoryBob {
+    // Создание Binder
+    // inner - значит имеет ссылку на родителя, где определен
+    // т.е. на CoolStoryBobService
+    inner class LocalBinder : Binder() {
+        fun getBob(): CoolStoryBob = this@CoolStoryBobService
+    }
+    private val binder = LocalBinder()
+    private val _stories = MutableSharedFlow<String>(
+        extraBufferCapacity = 1, onBufferOverflow = BufferOverflow.DROP_OLDEST
+    )
+    override val stories: SharedFlow<String> = _stories.asSharedFlow()
+
+
     override fun onCreate() {
         super.onCreate()
         Log.d("CoolStoryBobService", "onCreate: ")
     }
-    override fun onStartCommand(intent: Intent?, flags: Int, startId: Int): Int
-    {
+    override fun onStartCommand(intent: Intent?, flags: Int, startId: Int): Int {
     // имитация работы в потоке
-        Thread {
-            Thread.sleep(3000L)
-            Log.d("CoolStoryBobService","onStartCommand: complete!")
-        stopSelf()
-        }.start()
-        return START_STICKY
-    }
-    override fun onBind(intent: Intent?): IBinder? {
-        return null // Не поддерживаем привязку
+    Thread {
+        repeat(10) {
+            data ->
+            // каждую секунду отправляем историю
+            _stories.tryEmit("Story $data")
+            Thread.sleep(10L)
+        }
+        Log.d("CoolStoryBobService", "onStartCommand: complete!")
+        stopSelf() }.start()
+        return START_STICKY }
+    override fun onBind(intent: Intent?): IBinder {
+        return binder
     }
     override fun onDestroy() {
         super.onDestroy()
--- app/src/main/java/com/example/lab5mobile/MainActivity.kt
+++ app/src/main/java/com/example/lab5mobile/MainActivity.kt
@@ -1,14 +1,27 @@
 package com.example.lab5mobile
 
+import android.content.ComponentName
+import android.content.Context
 import android.content.Intent
+import android.content.ServiceConnection
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
+import android.os.IBinder
+import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+import androidx.lifecycle.Lifecycle
+import androidx.lifecycle.lifecycleScope
+import androidx.lifecycle.repeatOnLifecycle
 import com.example.lab5mobile.databinding.ActivityMainBinding
+import kotlinx.coroutines.launch
 
 class MainActivity : AppCompatActivity() {
+
+    private val collStoryBobConnection = object : ServiceConnection {
+        override fun onServiceConnected(name: ComponentName?, service: IBinder?) {
+            val coolStoryBob = (service as CoolStoryBobService.LocalBinder).getBob()
+            onProcessBobStories(coolStoryBob) }
+        override fun onServiceDisconnected(name: ComponentName?) { }
+    }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         val binding = ActivityMainBinding.inflate(layoutInflater)
@@ -16,6 +29,15 @@ class MainActivity : AppCompatActivity() {
         binding.startService.setOnClickListener {
             val intent = Intent(this, CoolStoryBobService::class.java)
             startService(intent)
+            bindService(intent, collStoryBobConnection, Context.BIND_AUTO_CREATE) }
+    }
+
+    private fun onProcessBobStories(coolStoryBob: CoolStoryBob) {
+        lifecycleScope.launch {
+            repeatOnLifecycle(Lifecycle.State.STARTED) {
+                coolStoryBob.stories.collect {
+                    story -> Log.d("MainActivity", "onProcessBobStories: $story") }
+            }
         }
     }
 }
