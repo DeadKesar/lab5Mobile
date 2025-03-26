279a8b9bb9ecafe24fcf2939c19ba2a3e04f8ed1
DeadKesar <50805926+DeadKesar@users.noreply.github.com>
2025-03-26 19:49:18 +0300

    добавили нотификацию... как будто бы что-то ещё должно быть, но я тупой и не понял...

--- app/src/main/java/com/example/lab5mobile/MainActivity.kt
+++ app/src/main/java/com/example/lab5mobile/MainActivity.kt
@@ -1,48 +1,95 @@
 package com.example.lab5mobile
 
-import android.content.ComponentName
-import android.content.Context
-import android.content.Intent
-import android.content.ServiceConnection
+import android.Manifest
+import android.app.NotificationManager
+import android.content.pm.PackageManager
+import android.os.Build
 import android.os.Bundle
-import android.os.IBinder
-import android.util.Log
+import android.widget.Toast
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AppCompatActivity
-import androidx.lifecycle.Lifecycle
-import androidx.lifecycle.lifecycleScope
-import androidx.lifecycle.repeatOnLifecycle
 import com.example.lab5mobile.databinding.ActivityMainBinding
-import kotlinx.coroutines.launch
+import android.app.Notification
+import android.app.NotificationChannel
+import androidx.core.app.NotificationCompat
 
 class MainActivity : AppCompatActivity() {
-
-    private val collStoryBobConnection = object : ServiceConnection {
-        override fun onServiceConnected(name: ComponentName?, service: IBinder?) {
-            val coolStoryBob = (service as CoolStoryBobService.LocalBinder).getBob()
-            onProcessBobStories(coolStoryBob)
-        }
-        override fun onServiceDisconnected(name: ComponentName?) {
-
-        }
-    }
-
+    // launcher для запроса разрешения
+    private val requestPostNotification = registerForActivityResult(
+        ActivityResultContracts.RequestPermission(),
+        ::onPostNotificationGranted )
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         val binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
-        binding.startService.setOnClickListener {
-            val intent = Intent(this, CoolStoryBobService::class.java)
-            startService(intent)
-            bindService(intent, collStoryBobConnection,
-                Context.BIND_AUTO_CREATE) }
-    }
-
-    private fun onProcessBobStories(coolStoryBob: CoolStoryBob) {
-        lifecycleScope.launch {
-            repeatOnLifecycle(Lifecycle.State.STARTED) {
-                coolStoryBob.stories.collect {
-                    story -> Log.d("MainActivity", "onProcessBobStories: $story") }
-            }
+        onSetupNotificationChannel()
+        binding.notify.setOnClickListener {
+            onSendNotification()
         }
     }
+    private fun createNotificationForBob(): Notification {
+        return NotificationCompat.Builder(this,
+            CoolStoryBobNotification.CHANNEL_ID)
+            .setSmallIcon(R.drawable.baseline_cookie_24)// иконка
+            .setShowWhen(true) // время
+            .setOngoing(true) // может ли пользователь его сам убрать
+        .setContentText("Story from bob") // текст
+        .build()
+    }
+    private fun onSetupNotificationChannel() {
+        // проверяем текущие устройство
+        // если Android ниже 8, то просто возвращаемся, ибо
+        // channel не нужен
+        if (Build.VERSION.SDK_INT < Build.VERSION_CODES.O) {
+            return
+        }
+        // создание канала
+        val channel = NotificationChannel(
+            CoolStoryBobNotification.CHANNEL_ID,
+            getString(R.string.bob_notification),
+            NotificationManager.IMPORTANCE_DEFAULT
+        )
+        // установка его
+        val manager = getSystemService(NotificationManager::class.java)
+        manager.createNotificationChannel(channel)
+    }
+
+
+    // отправка уведомления
+    private fun onSendNotification() {
+        if (!isPostNotificationGranted()) {
+            requestPostNotification()
+        }
+        val notification = createNotificationForBob()
+        val notificationManager = getSystemService(NotificationManager::class.java)
+        notificationManager.notify(CoolStoryBobNotification.NOTIFICATION_ID, notification)
+    }
+    // запрос разрешения
+    private fun requestPostNotification() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
+            requestPostNotification.launch(Manifest.permission.POST_NOTIFICATIONS)
+        }
+    }
+    // проверка есть ли разрешение
+    private fun isPostNotificationGranted(): Boolean {
+        return if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
+            checkSelfPermission(Manifest.permission.POST_NOTIFICATIONS) ==
+                    PackageManager.PERMISSION_GRANTED
+        } else {
+            true
+        }
+    }
+    // получение результата разрешения
+    private fun onPostNotificationGranted(isGranted: Boolean) {
+        if (!isGranted) {
+            Toast.makeText(this,
+                "Permission not granted",
+                Toast.LENGTH_SHORT).show()
+            return }
+        onSendNotification()
+    }
+
+
+
+
 }
